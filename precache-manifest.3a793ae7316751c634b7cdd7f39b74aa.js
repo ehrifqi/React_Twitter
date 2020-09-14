647ecde8103a5bc1b4a3887522b1b7c570a703ee
ehrifqi <torrosel@gmail.com>
2020-09-14 18:51:00 +0700

    Updates

---
+++ precache-manifest.3a793ae7316751c634b7cdd7f39b74aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c874899007b5cc7f962c6d1b4d9e73b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3314027c28806386ef54",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2b3d7256f0c812ab828",
+    "url": "/static/js/2.ab65c84a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab65c84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3314027c28806386ef54",
+    "url": "/static/js/main.1ede50a3.chunk.js"
+  },
+  {
+    "revision": "723b8de4144d7b4aea6d",
+    "url": "/static/js/runtime-main.562f75cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
