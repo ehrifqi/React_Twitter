d8135aa0e8b7442aebce6e416f22f37d845e5ea0
ehrifqi <torrosel@gmail.com>
2020-09-22 18:39:37 +0700

    Set the widgets and feed component at early stage

---
+++ src/Feed.js
@@ -0,0 +1,74 @@
+import React from "react";
+import "./Feed.css";
+
+function Feed() {
+  return (
+    <div className="feed">
+      {/* Header */}
+      <div className="feed__header">
+        <h2>Home</h2>
+      </div>
+
+      {/* TweetBox */}
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      <h3>abc</h3>
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+      {/* Post */}
+    </div>
+  );
+}
+
+export default Feed;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -10,21 +10,26 @@ import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
 import ListAltIcon from "@material-ui/icons/ListAlt";
 import PermIdentityIcon from "@material-ui/icons/PermIdentity";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
 
 function Sidebar() {
   return (
     <div className="sidebar">
       {/* Twitter Icon*/}
 
-      <SidebarOption Icon={TwitterIcon} text="Twitter" />
-      <SidebarOption Icon={HomeIcon} text="Home" />
+      <TwitterIcon className="sidebar__twitterIcon" />
+      <SidebarOption active Icon={HomeIcon} text="Home" />
       <SidebarOption Icon={SearchIcon} text="Explore" />
       <SidebarOption Icon={NotificationsNoneIcon} text="Notifications" />
-      <SidebarOption Icon={MailOutlineIcon} text="Home" />
-      <SidebarOption Icon={BookmarkBorderIcon} text="Explore" />
-      <SidebarOption Icon={ListAltIcon} text="Notifications" />
-      <SidebarOption Icon={PermIdentityIcon} text="Home" />
-      <SidebarOption Icon={MoreHorizIcon} text="Explore" />
+      <SidebarOption Icon={MailOutlineIcon} text="Messages" />
+      <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks" />
+      <SidebarOption Icon={ListAltIcon} text="Lists" />
+      <SidebarOption Icon={PermIdentityIcon} text="Profile" />
+      <SidebarOption Icon={MoreHorizIcon} text="More" />
+
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
     </div>
   );
 }
